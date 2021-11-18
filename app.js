bcacd585c2550613901a76c36e06406842c4f3b2
Khushnood Asif <khushnood99@gmail.com>
2021-11-18 20:21:30 +0000

    changes

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ passport.use(new GoogleStrategy({
    clientID: process.env.CLIENT_ID,
    clientSecret: process.env.CLIENT_SECRET,
    callbackURL: "http://localhost:3000/auth/google/secrets",
-   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+   userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo"
 },
    function(accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {

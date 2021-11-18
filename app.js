84a9679d90b072a2797032627fef85e0e0790405
Khushnood Asif <khushnood99@gmail.com>
2021-11-18 04:27:48 +0000

    fixed submit issue

--- app.js
+++ app.js
@@ -90,11 +90,40 @@ app.get("/register", function (req, res) {
 
 
 app.get("/secrets", function (req, res) {
+   User.find({"secret": {$ne: null}}, function (err, foundUsers) {
+      if (err) {
+         console.log(err);
+      } else {
+         if (foundUsers) {
+            res.render("secrets", { usersWithSecrets: foundUsers });
+         }
+      }
+   });
+});
+
+app.get("/submit", function (req, res) {
    if (req.isAuthenticated()) {
-      res.render("secrets");
+      res.render("submit");
    } else {
       res.redirect("/login");
    }
+});
+
+app.post("/submit", function (req, res) {
+   const submittedSecret = req.body.secret;
+   
+   User.findById(req.user.id, function (err, foundUser) {
+      if (err) {
+         console.log(err);
+      } else {
+         if (foundUser) {
+            foundUser.secret = submittedSecret;
+            foundUser.save(function () {
+               res.redirect("/secrets");
+            });
+         }
+      }
+   });
 });
 
 app.get("/logout", function (req, res) {
